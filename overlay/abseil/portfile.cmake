2a8b0530a72ce2a0a4ccba4f3a31a7059d675fff
msd-sajjad007 <socialnetwork.sajad@gmail.com>
2026-06-15 09:10:55 -0700

    fix: correct abseil overlay - exact upstream files with only vcpkg_fixup_pkgconfig removed

--- overlay/abseil/portfile.cmake
+++ overlay/abseil/portfile.cmake
@@ -1,33 +1,69 @@
-# Overlay portfile for abseil - identical to upstream except vcpkg_fixup_pkgconfig is removed
-# because the MSYS2 pkgconf package it requires returns 404 on all mirrors.
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO abseil/abseil-cpp
     REF "${VERSION}"
-    SHA512 516952bc3d9658d56d2de2f7e87ea8e5f55c77f4c5970de1ca84f93d97df9c6e3a6b57e869f9c3ca6e6a16c5cbf3eb5ebf50ad3e4d14a928e4c5565b44e4b3a8d
+    SHA512 f5012885d6b6844a9cf5ed92ad5468b8757db33dfe1364bfb232fff928e06c550c7eb4557f45186a8ac4d18b178df9be267681abab4a6de40823b574afbe9960
     HEAD_REF master
     PATCHES
+        003-force-cxx-17.patch
+        fix-heterogeneous_lookup_testing-target.patch
+        fix-mingw-dll.patch
+        fix-gcc13-constexpr-function-pointer.patch
 )
+
+set(ABSL_STATIC_RUNTIME_OPTION "")
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_CRT_LINKAGE STREQUAL "static")
+    set(ABSL_STATIC_RUNTIME_OPTION "-DABSL_MSVC_STATIC_RUNTIME=ON")
+endif()
+
+set(ABSL_MINGW_OPTIONS "")
+if(VCPKG_TARGET_IS_MINGW)
+    set(ABSL_MINGW_OPTIONS "-DLIBRT=LIBRT-NOTFOUND"
+        "-DCMAKE_CXX_FLAGS=-D____FIReference_1_boolean_INTERFACE_DEFINED__")
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+        vcpkg_list(APPEND ABSL_MINGW_OPTIONS "-DABSL_BUILD_MONOLITHIC_SHARED_LIBS=ON")
+    endif()
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DABSL_PROPAGATE_CXX_STD=ON
-        -DABSL_USE_EXTERNAL_GOOGLETEST=OFF
         -DABSL_BUILD_TESTING=OFF
-        -DABSL_ENABLE_INSTALL=ON
-        -DCMAKE_CXX_STANDARD=17
+        -DABSL_BUILD_TEST_HELPERS=OFF
+        ${ABSL_STATIC_RUNTIME_OPTION}
+        ${ABSL_MINGW_OPTIONS}
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/absl)
-vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(PACKAGE_NAME absl CONFIG_PATH lib/cmake/absl)
 
-# Note: vcpkg_fixup_pkgconfig() intentionally omitted.
-# It tries to download MSYS2 pkgconf which returns 404.
+if(VCPKG_TARGET_IS_IOS OR VCPKG_TARGET_IS_OSX)
+    file(APPEND "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/absl_time.pc" "Libs.private: -framework CoreFoundation\n")
+    if(NOT VCPKG_BUILD_TYPE)
+        file(APPEND "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/absl_time.pc" "Libs.private: -framework CoreFoundation\n")
+    endif()
+endif()
+
+# vcpkg_fixup_pkgconfig() intentionally removed:
+# It downloads MSYS2 pkgconf which returns 404 on all mirrors.
 # pkgconfig files are not required for CMake consumers.
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_copy_pdbs()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/include/absl/copts"
+                    "${CURRENT_PACKAGES_DIR}/include/absl/strings/testdata"
+                    "${CURRENT_PACKAGES_DIR}/include/absl/time/internal/cctz/testdata"
+)
+
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/absl/base/config.h" "defined(ABSL_CONSUME_DLL)" "1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/absl/base/internal/thread_identity.h" "defined(ABSL_CONSUME_DLL)" "1")
+endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
