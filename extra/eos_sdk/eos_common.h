caeb19f02f881f5132df313c2ab2b70bbaf3f040
msd-sajjad007 <socialnetwork.sajad@gmail.com>
2026-06-17 05:33:58 -0700

    fix: add EOS_AccountId backward-compat typedef and include all versioned common headers

--- extra/eos_sdk/eos_common.h
+++ extra/eos_sdk/eos_common.h
@@ -5,3 +5,9 @@
 #include "eos_common1.16.4.h"
 #include "eos_common1.14.1.h"
 #include "eos_common1.3.1.h"
+
+// Backward compatibility: pre-1.5 SDK used EOS_AccountId instead of EOS_EpicAccountId.
+// Old versioned headers (eos_auth_types1.3.1.h, eos_ecom_types1.1.0.h, etc.) still reference it.
+#ifndef EOS_AccountId
+typedef EOS_EpicAccountId EOS_AccountId;
+#endif
