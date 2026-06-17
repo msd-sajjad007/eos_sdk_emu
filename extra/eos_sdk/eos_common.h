1f9fd1228e9568826c8a2864f420fe71707f6ef8
msd-sajjad007 <socialnetwork.sajad@gmail.com>
2026-06-17 05:14:33 -0700

    fix: restore eos_common.h to include all three versioned headers (1.3.1, 1.14.1, 1.16.4)

--- extra/eos_sdk/eos_common.h
+++ extra/eos_sdk/eos_common.h
@@ -3,11 +3,5 @@
 #pragma once
 
 #include "eos_common1.16.4.h"
-
-// [FIX] Guard EOS_PAGINATION_API_LATEST to suppress redefinition warning
-// when both eos_common.h and eos_common1.16.4.h are included in the same TU.
-// EOS_PAGINATION_API_LATEST is already defined inside eos_common1.16.4.h;
-// this guard prevents a duplicate-macro warning on MSVC.
-#ifndef EOS_PAGEQUERY_API_LATEST
-#define EOS_PAGEQUERY_API_LATEST 1
-#endif
+#include "eos_common1.14.1.h"
+#include "eos_common1.3.1.h"
